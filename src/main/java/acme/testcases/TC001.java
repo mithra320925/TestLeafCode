5a354575e9f9149a1a0cf317efc8725df4b5c095
mithra <mithra@DESKTOP-5323T0U>
2019-10-12 15:29:37 +0530

    TestLeafProject

---
+++ src/main/java/acme/testcases/TC001.java
@@ -0,0 +1,36 @@
+package acme.testcases;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+import org.testng.annotations.Test;
+
+public class TC001 {
+
+	@Test
+	public void acmeTestCase() {
+		System.setProperty("webdriver.chrome.driver", "./drivers/chromedriver.exe");
+		ChromeDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("https://acme-test.uipath.com/account/login");
+		driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
+		driver.findElementById("email").sendKeys("kumar.testleaf@gmail.com");
+		driver.findElementById("password").sendKeys("leaf@12");
+		driver.findElementById("buttonLogin").click();
+		WebElement vendor = driver.findElementByXPath("//div[@class='dropdown'][5]");
+		Actions builder = new Actions(driver);
+		builder.moveToElement(vendor).perform();
+		driver.findElementByLinkText("Search for Vendor").click();
+		driver.findElementById("vendorName").sendKeys("Blue Lagoon");
+		driver.findElementById("buttonSearch").click();
+
+		String text = driver.findElementByXPath("//tr[2]/td[5]").getText();
+		System.out.println("Country is " + text);
+
+		driver.findElementByLinkText("Log Out").click();
+		driver.close();
+
+	}
+}
